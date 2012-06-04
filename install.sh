3d5f9d5781ac1e7fbfb5ce906ff37ad7ef15b861
Felipe Oliveira <felipe.oliveira@abril.com.br>
2012-06-04 17:07:35 -0300

    adjust install script

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -x
-
 for f in `ls | grep -v install.sh`;
 do
     ln -snf $PWD/$f $HOME/.$f
 done
 
-#source $HOME/.bash_profile
+source $HOME/.bash_profile
 
 echo "Installed!"
