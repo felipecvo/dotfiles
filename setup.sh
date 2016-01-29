2f434cc3b307224fe0dfbc689d07feb85edd0ec5
Felipe Oliveira <felipecvo@gmail.com>
2016-01-29 21:10:19 -0200

    OSX only

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-sudo softwareupdate -ia
+OS=$(uname -s)
 
-xcode-select --install
-
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-export PATH=/usr/local/bin:$PATH
-
-git clone https://github.com/felipecvo/dotfiles.git ~/.dotfiles
-
-PUSHD ~/.dotfiles
-  brew tap Homebrew/bundle
-  brew install caskroom/cask/brew-cask
-  brew bundle
-POPD
+if [ "$OS" == "Darwin" ]; then
+  sudo softwareupdate -ia
+  
+  xcode-select --install
+  
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  export PATH=/usr/local/bin:$PATH
+  
+  git clone https://github.com/felipecvo/dotfiles.git ~/.dotfiles
+  
+  pushd ~/.dotfiles
+    brew tap Homebrew/bundle
+    brew install caskroom/cask/brew-cask
+    brew bundle
+  popd
+fi
